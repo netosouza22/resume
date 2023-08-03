24767422f8a590dcfe002134d023c536cff8d6fa
Neto Sousa <netosouzacp@gmail.com>
2023-08-02 22:08:38 -0300

    feat: add AboutMe component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
+import AboutMe from '@/components/AboutMe';
 import Header from '@/components/Header';
 import TitleSection from '@/components/TitleSection';
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen min-w-screen bg-white font-poppins">
-      <div className="m-auto max-w-6xl">
+    <main className="min-h-screen min-w-screen bg-white text-gray-10 font-poppins">
+      <div className="m-auto max-w-6xl px-4 md:px-12 lg:px-0">
         <Header />
         <TitleSection />
+        <AboutMe />
       </div>
     </main>
   )
