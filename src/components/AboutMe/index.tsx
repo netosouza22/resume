f2134abed9cb78600905059ae937e2df9ca41259
Neto Sousa <netosouzacp@gmail.com>
2023-08-08 11:48:54 -0300

    styles: add maxwidth. change space between elemenents and behavior of elements and different screen sizes

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -5,10 +5,10 @@ import SquareIcon from "../common/SquareIcon";
 
 export default function AboutMe() {
   return (
-    <section className="relative">
+    <section className="m-auto max-w-6xl relative mb-[15px] md:mb-[3.75rem]" id="about-me">
       <SectionTitle title="sobre mim" />
 
-      <div className="flex gap-4 flex-wrap lg:flex-nowrap items-center md:gap-6">
+      <div className="flex gap-4 flex-wrap lg:flex-nowrap items-center md:gap-14 lg:gap-28">
         <span className="text-sm text-gray-7 font-normal max-w-2xl md:text-base">
           Sou um profissional dedicado e curioso pela área de desenvolvimento web,
           particularmente relacionado ao <span className="font-bold text-pb-dark">front end e UI/UX design</span>,
@@ -18,7 +18,7 @@ export default function AboutMe() {
           sempre buscando evoluir minhas habilidades técnicas e interpessoais,
           através de constante estudo.
         </span>
-        <aside className="flex gap-10 flex-wrap justify-center md:justify-end">
+        <aside className="flex gap-5 flex-wrap justify-center md:gap-10 ">
           {
             tecnologyIcons.map((item) => {
               return (<SquareIcon key={item.label} label={item.label} icon={item.icon}></SquareIcon>)
