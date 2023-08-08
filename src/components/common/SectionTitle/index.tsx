35307a30fffb633069434fd21985f9109e8a1fac
Neto Sousa <netosouzacp@gmail.com>
2023-08-08 11:52:08 -0300

    feat: change color title based in a props

--- src/components/common/SectionTitle/index.tsx
+++ src/components/common/SectionTitle/index.tsx
@@ -1,12 +1,13 @@
 
 interface ISectionTitle {
   title: string;
+  colorStyle?: "dark" | "light";
 }
 
-export default function SectionTitle({ title }: ISectionTitle) {
+export default function SectionTitle({ title, colorStyle = 'dark' }: ISectionTitle) {
   return (
-    <div className="">
-      <h1 className="relative text-gray-10 font-bold capitalize z-20 text-[3rem]  rounded
+    <div className={colorStyle === "dark" ? `text-gray-10` : `text-gray-light`}>
+      <h1 className="relative  font-bold capitalize z-10 text-[3rem]  rounded
         before:content-shape-mark before:absolute before:-z-20 before:-top-1 before:-left-5
         md:text-[4rem]
         md:before:-top-4 md:before:-left-5
