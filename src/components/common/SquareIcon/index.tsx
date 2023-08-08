ec0603cecfb299c5c4955171f53edb74570d0435
Neto Sousa <netosouzacp@gmail.com>
2023-08-08 11:55:07 -0300

    styles: remove square behind the icon

--- src/components/common/SquareIcon/index.tsx
+++ src/components/common/SquareIcon/index.tsx
@@ -9,10 +9,9 @@ export default function SquareIcon({ label, icon }: ISquareIcon) {
 
 
   return (
-    <div className="w-[3.75rem] h-[3.75rem] bg-gray-1 flex items-center justify-center">
-      <div title={label}>
-        {icon}
-      </div>
+    <div title={label}>
+      {icon}
     </div>
+
   )
 }
