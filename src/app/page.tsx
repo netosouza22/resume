9c5a5632eaad869175407923dcee5cefca755abc
Neto Sousa <netosouzacp@gmail.com>
2023-08-08 11:53:02 -0300

    feat: add new component sections and remove max-width property

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 import AboutMe from '@/components/AboutMe';
+import Education from '@/components/Education';
 import Header from '@/components/Header';
 import TitleSection from '@/components/TitleSection';
+import WorkExperience from '@/components/WorkExperience';
 
 
 export default function Home() {
   return (
     <main className="min-h-screen min-w-screen bg-white text-gray-10 font-poppins">
-      <div className="m-auto max-w-6xl px-4 md:px-12 lg:px-4 xl:px-0">
+      <div className="px-4 md:px-12 lg:px-4 xl:px-0">
         <Header />
         <TitleSection />
         <AboutMe />
+        <WorkExperience />
+        <Education />
       </div>
     </main>
   )
