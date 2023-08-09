5b77e99d99c4b47b63ab6781a8390b377f5ea81a
Neto Sousa <netosouzacp@gmail.com>
2023-08-09 11:50:01 -0300

    refactor: add div to englobe some sections and add new sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import AboutMe from '@/components/AboutMe';
+import ContactMe from '@/components/ContactMe';
 import Education from '@/components/Education';
 import Header from '@/components/Header';
 import TitleSection from '@/components/TitleSection';
@@ -8,12 +9,15 @@ import WorkExperience from '@/components/WorkExperience';
 export default function Home() {
   return (
     <main className="min-h-screen min-w-screen bg-white text-gray-10 font-poppins">
-      <div className="px-4 md:px-12 lg:px-4 xl:px-0">
-        <Header />
-        <TitleSection />
-        <AboutMe />
-        <WorkExperience />
+      <div>
+        <div className="px-4 md:px-12 lg:px-4 xl:px-0">
+          <Header />
+          <TitleSection />
+          <AboutMe />
+          <WorkExperience />
+        </div>
         <Education />
+        <ContactMe />
       </div>
     </main>
   )
