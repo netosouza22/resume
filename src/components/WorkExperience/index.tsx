598878dabce4b30cca8d071750d45dcfa880321b
Neto Sousa <netosouzacp@gmail.com>
2023-08-10 11:58:16 -0300

    styles: change gap and margin bottom and top of section

--- src/components/WorkExperience/index.tsx
+++ src/components/WorkExperience/index.tsx
@@ -4,10 +4,10 @@ import WorkExperienceCard from "../common/WorkExperienceCard";
 
 export default function WorkExperience() {
   return (
-    <section className="m-auto max-w-6xl flex flex-col gap-[1.875rem] md:gap-[3.75rem]">
+    <section id="work-experience" className="m-auto max-w-6xl my-[3.75rem] flex flex-col gap-[1.25rem] md-[1.875rem] md:gap-[2.5rem]">
       <SectionTitle title="Experiência" />
 
-      <div>
+      <div className="flex flex-col gap-4 md:gap-5">
         {
           cvResume["work-experience"].map((workExperience, index) => {
 
