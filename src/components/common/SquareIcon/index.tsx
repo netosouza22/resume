29e357fe8923e286f04445a9f5ba3d522de9d533
Neto Sousa <netosouzacp@gmail.com>
2023-08-10 16:52:34 -0300

    feat: add link to access github

--- src/components/common/SquareIcon/index.tsx
+++ src/components/common/SquareIcon/index.tsx
@@ -9,9 +9,24 @@ export default function SquareIcon({ label, icon }: ISquareIcon) {
 
 
   return (
-    <div title={label}>
-      {icon}
-    </div>
+    <>
+      {
+        label === "GIT" ?
+          <a
+            title={label}
+            className="relative hover:scale-105 hover:cursor-pointer z-10
+              before:absolute before:w-3 before:h-3 before:bg-pb before:z-20 before:top-0 before:right-0
+              before:rounded-full before:animate-pulse before:animate-bounce"
+            href="https://github.com/netosouza22"
+            target="_blank" >
+            {icon}
+          </a>
+          :
+          <div title={label} >
+            {icon}
+          </div>
+      }
+    </>
 
   )
 }
