de73bf578c4a56ca67da6e0b899d83a8762873ca
Neto Sousa <netosouzacp@gmail.com>
2023-08-10 16:39:20 -0300

    feat: add ubtton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,25 @@ import Education from '@/components/Education';
 import Header from '@/components/Header';
 import TitleSection from '@/components/TitleSection';
 import WorkExperience from '@/components/WorkExperience';
+import ToTopButton from '@/components/common/ToTopButton';
 
 
 export default function Home() {
   return (
-    <main className="min-h-screen min-w-screen bg-white text-gray-10 font-poppins">
-      <div>
-        <div className="px-4 md:px-12 lg:px-4 xl:px-0">
-          <Header />
-          <TitleSection />
-          <AboutMe />
-          <WorkExperience />
+    <>
+      <main className="min-h-screen min-w-screen bg-white text-gray-10 font-poppins">
+        <div>
+          <div className="px-4 md:px-12 lg:px-4 xl:px-0">
+            <Header />
+            <TitleSection />
+            <AboutMe />
+            <WorkExperience />
+          </div>
+          <Education />
+          <ContactMe />
         </div>
-        <Education />
-        <ContactMe />
-      </div>
-    </main>
+      </main>
+      <ToTopButton />
+    </>
   )
 }
